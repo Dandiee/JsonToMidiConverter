3c3e17cf91fc5875690207b4d3cac419d005dbdf
Daniel Koczur <dkoczur@slb.com>
2025-11-20 08:03:16 +0100

    refact part 2

---
+++ JsonToMidiConverter/Extensions.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Melanchall.DryWetMidi.Interaction;
+
+namespace JsonToMidiConverter;
+
+public static class Extensions
+{
+    public static readonly IReadOnlyDictionary<Type, TimeSpanType> TimeSpanTypeMapping =
+        new Dictionary<Type, TimeSpanType>
+        {
+            [typeof(BarBeatFractionTimeSpan)] = TimeSpanType.BarBeatFraction,
+            [typeof(BarBeatTicksTimeSpan)] = TimeSpanType.BarBeatTicks,
+            [typeof(MetricTimeSpan)] = TimeSpanType.Metric,
+            [typeof(MidiTimeSpan)] = TimeSpanType.Midi,
+            [typeof(MusicalTimeSpan)] = TimeSpanType.Musical,
+        };
+
+    public static TTimeSpan AddTicks<TTimeSpan>(this TTimeSpan time, long tick, TempoMap tempoMap)
+        where TTimeSpan : ITimeSpan
+    {
+        var clone = time.Clone();
+        var tickTime = TimeConverter.ConvertTo(Math.Abs(tick), TimeSpanTypeMapping[time.GetType()], tempoMap);
+
+        if (tick >= 0)
+        {
+            clone = clone.Add(tickTime, TimeSpanMode.TimeLength);
+        }
+        else
+        {
+            clone = clone.Subtract(tickTime, TimeSpanMode.TimeLength);
+        }
+
+            return (TTimeSpan)clone;
+    }
+}
