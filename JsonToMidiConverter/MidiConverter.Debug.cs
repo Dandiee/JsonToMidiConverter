ea4d1ba2cf834a465fe49e388049acf708dd9c80
Daniel Koczur <dkoczur@slb.com>
2025-11-21 14:11:45 +0100

    asdasdasd

---
+++ JsonToMidiConverter/MidiConverter.Debug.cs
@@ -0,0 +1,93 @@
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using Melanchall.DryWetMidi.MusicTheory;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualBasic;
+using Note = Melanchall.DryWetMidi.Interaction.Note;
+
+namespace JsonToMidiConverter;
+
+internal static partial class MidiConverter
+{
+    
+
+    public static (TimedEvent Event, NoteContext Ctx) Add(this IList<TimedEvent> events, MidiEvent midiEvent, ITimeSpan time,
+        NoteContext ctx, int? channelOverride = null, int? partId = null, int? noteNumberOverride = null)
+    {
+
+        var origNoteNumber = ctx.Note?.NoteNumber;
+        if (noteNumberOverride != null)
+        {
+            ctx.Note.NoteNumber = noteNumberOverride.Value;
+        }
+
+        if (midiEvent is ChannelEvent channelEvent)
+        {
+            channelEvent.Channel = (FourBitNumber)(channelOverride ?? GetNoteChannel(ctx.Note.Part, ctx.Note!));
+        }
+
+        var tickTime = TimeConverter.ConvertFrom(time, ctx.TempoMap);
+        var eventType = midiEvent.GetType();
+
+        if (!SuspenseValidation)
+        {
+            var pid = partId ?? ctx.Note.Part.Index;
+
+            if (pid < 10)
+            {
+                var referenceChunk = ReferenceData[pid];
+                var referenceEvent = referenceChunk[events.Count];
+                var areTheSameType = referenceEvent.Event.GetType() == eventType;
+                Debug.Assert(areTheSameType);
+
+                if (!(midiEvent is PitchBendEvent pitch && pitch.PitchValue == 8888))
+                {
+                    var warning =
+                        $"Time mismatch at Index {events.Count} of {eventType.Name}, Expected = {referenceEvent.AbsoluteTime} vs Actual = {tickTime}";
+                    var diff = referenceEvent.AbsoluteTime - tickTime;
+                    if (Math.Abs(diff) > 8)
+                    {
+                        Debug.Assert(referenceEvent.AbsoluteTime == tickTime, warning);
+                    }
+                }
+
+                if (areTheSameType)
+                {
+                    var props = eventType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var prop in props)
+                    {
+                        var propName = prop.Name;
+                        var referenceValue = prop.GetValue(referenceEvent.Event)!;
+                        var actualValue = prop.GetValue(midiEvent)!;
+
+                        if (propName != "DeltaTime" && propName != "Velocity" && propName != "PitchValue")
+                        {
+                            if (!(propName == "PitchValue" && actualValue.ToString() == "8888"))
+                            {
+                                Debug.Assert(referenceValue.ToString() == actualValue.ToString(), propName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        var newEvent = new TimedEvent(midiEvent, tickTime);
+        events.Add(newEvent);
+        if (ctx.Note != null)
+        {
+            ctx.Note.Events.Add(newEvent);
+        }
+
+        if (noteNumberOverride != null)
+        {
+            ctx.Note.NoteNumber = origNoteNumber.Value;
+        }
+
+        return (newEvent, ctx);
+    }
+
+}
